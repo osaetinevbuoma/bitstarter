b06abf043736eb797318b50442b80191134ab409
Osaetin Evbuoma <osaetinevbuoma@gmail.com>
2013-07-05 20:37:44 +0100

    Trying new web.js

--- web.js
+++ web.js
@@ -2,13 +2,15 @@ var express = require('express');
 var fs = require('fs');
 
 var file = fs.readFileSync("index.html", "utf8"); //second parameter converts file read to string
-var buffer = new Buffer(256);
+//var buffer = new Buffer(256);
+var bufer = new Buffer(file, "utf8");
 var len = buffer.write(file, 0); //calculates the length of the input and writes to buffer
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString("utf8", 0, len)); //converts the buffer to string
+  //response.send(buffer.toString("utf8", 0, len)); //converts the buffer to string
+  response.send(buffer.toString("utf8"));
 });
 
 var port = process.env.PORT || 5000;
