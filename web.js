b1c3141ead43a3d541bc7c6e4ec7a9644b0aee8e
Osaetin Evbuoma <osaetinevbuoma@gmail.com>
2013-07-05 11:50:19 +0100

    Web.js with fs and buffer

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = requre('fs');
+
+var index = fs.readFileSync("index.html");
+var buffer = new Buffer(256);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  //response.send('Hello World2!');
+  respone.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
