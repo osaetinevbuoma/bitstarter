cbe33412c0b9c9c0ade5f731f0f422e34411535c
Osaetin Evbuoma <osaetinevbuoma@gmail.com>
2013-07-05 14:31:22 +0100

    Updated web.js again

--- web.js
+++ web.js
@@ -3,13 +3,13 @@ var fs = require('fs');
 
 var file = fs.readFileSync("index.html");
 var buffer = new Buffer(256);
-var len = buffer.write("Hello World", 0);
+buffer.write("Hello World", 0);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   //response.send('Hello World2!');
-  respone.send(buffer.toString("utf8", 0, len));
+  respone.send(buffer.toString("utf8"));
 });
 
 var port = process.env.PORT || 5000;
