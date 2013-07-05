00179d205bc126a5b598cabf23799a408fc4226b
Osaetin Evbuoma <osaetinevbuoma@gmail.com>
2013-07-05 19:10:32 +0100

    Completed web.js from fs

--- web.js
+++ web.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var fs = require('fs');
 
-var file = fs.readFileSync("index.html", "utf8");
+var file = fs.readFileSync("index.html", "utf8"); //second parameter converts file read to string
 var buffer = new Buffer(256);
-var len = buffer.write(file, 0);
+var len = buffer.write(file, 0); //calculates the length of the input and writes to buffer
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  //response.send('Hello World2!');
-  response.send(buffer.toString("utf8", 0, len));
+  response.send(buffer.toString("utf8", 0, len)); //converts the buffer to string
 });
 
 var port = process.env.PORT || 5000;
