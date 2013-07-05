e99627e5edbdb06ae7e9db10e07cb9646dcd19f1
Osaetin Evbuoma <osaetinevbuoma@gmail.com>
2013-07-05 11:07:44 +0100

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
-var app = express.createServer(express.logger());
+//var app = express.createServer(express.logger());
+var app = express();
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
